75012c05aef71a430dd7fde6121b8b34eb7fe7e5
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2021-01-11 13:38:12 +0300

    first commit

--- portfolio/data.php
+++ portfolio/data.php
@@ -2,11 +2,11 @@
 //при обращении к файлу, он будет возвращать определенную информацию через ключевое слово return
 return [
     'about' => [
-        'name' => 'Иван Иванов',
-        'post' => 'менеджер',
-        'email' => 'adress@mail.ru',
+        'name' => 'Попенков Антон',
+        'post' => 'Веб-разработчик',
+        'email' => 'popenkovaa@yandex.ru',
         'phone' => '1-222-33-44',
-        'site' => 'google.com'
+        'site' => 'popenkov.ru'
     ],
     'education' => [
         [
@@ -16,12 +16,90 @@ return [
             'yearEnd' => 2018,
         ],
         [
-            'faculty' => 'Random faculty',
-            'university' => 'Random university',
+            'faculty' => 'Random faculty 2',
+            'university' => 'Random university 2',
             'yearStart' => 2011,
             'yearEnd' => 2015,
         ],
 
-    ]
+    ],
+    'languages' => [
+        'Russian', 'English', 'German'
+    ],
+
+    'interests' => [
+        'увлечение 1', 'увлечение 2'
+    ],
+
+    'aboutCareer' => 'lorem ipsum',
+
+    'career' => [
+        [
+            'post' => 'post 1',
+            'place' => 'city 1',
+            'duties' => 'duties 1',
+            'yearStart' => '2011',
+            'yearEnd' => '2015',
+        ],
+        [
+            'post' => 'post 2',
+            'place' => 'city 2',
+            'duties' => 'duties 2',
+            'yearStart' => '2015',
+            'yearEnd' => '2018',
+        ],
+        [
+            'post' => 'post 3',
+            'place' => 'city 3',
+            'duties' => 'duties 3',
+            'yearStart' => '2018',
+            'yearEnd' => '2021',
+        ],
+    ],
+
+    'projects' => [
+        [
+            'name' => 'project name 1',
+            'description' => 'project description 1'
+        ],
+        [
+            'name' => 'project name 2',
+            'description' => 'project description 2'
+        ],
+        [
+            'name' => 'project name 3',
+            'description' => 'project description 3'
+        ],
+        [
+            'name' => 'project name 4',
+            'description' => 'project description 4'
+        ],
+        [
+            'name' => 'project name 5',
+            'description' => 'project description 5'
+        ],
+    ],
+
+    'skills' => [
+        [
+            'name' => 'HTML & CSS',
+            'dataLevel' => 80
+        ],
+[
+    'name' => 'JavaScript',
+    'dataLevel' => 30
+],
+[
+    'name' => 'PHP',
+    'dataLevel' => 10
+],
+[
+    'name' => 'Photoshop',
+    'dataLevel' => 100
+],
+
+
+]
+
 
 ];
